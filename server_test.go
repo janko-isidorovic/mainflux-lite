fc8d133242ae30fcae1563dfbed04c5df2dd7c59
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2016-08-27 22:59:08 +0200

    Remove comments
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- server_test.go
+++ server_test.go
@@ -45,39 +45,3 @@ func TestServer(t *testing.T) {
 
 }
 
-/**
-func TestServer(t *testing.T) {
-	mfl := new(MainfluxLite)
-	expectedBody := `{"status":"OK"}`
-
-	recorder := httptest.NewRecorder()
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("http://localhost:7070/status", expectedBody), nil)
-	if err != nil {
-		t.Errorf("Failed to create request.")
-	}
-
-	// Config
-	cfg := config.Config{
-		HttpHost:"localhost",
-		HttpPort:7070,
-
-		// Mongo
-		MongoHost:"localhost",
-		MongoPort:27017,
-		MongoDatabase:"mainflux_test",
-	}
-
-	//mfl.ServeHTTP(recorder, req)
-	mfl.ServeHTTP(cfg)
-
-	switch recorder.Body.String() {
-	case expectedBody:
-		// body is equal so no need to do anything
-	default:
-		t.Errorf("Body (%s) did not match expectation (%s).",
-			recorder.Body.String(),
-			expectedBody)
-	}
-}
-*/
